ac3cc99a8fa7b6eddb6f72b65d05e574c2720d91
Mckmik <mpmckeever914@gmail.com>
2025-03-17 11:59:55 -0500

    search function

--- src/Routes/AllGames.jsx
+++ src/Routes/AllGames.jsx
@@ -7,6 +7,7 @@ function AllGames(){
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [filteredData, setFilteredData] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -19,6 +20,7 @@ function AllGames(){
             const parser = new XMLParser();
             const xmlData = parser.parseFromString(xml);
             setData(xmlData);
+            setFilteredData(xmlData.children);
             setLoading(false);
           } catch (e) {
             setError(e);
@@ -28,6 +30,22 @@ function AllGames(){
     
         fetchData();
       }, []);
+
+      const handleChange = e => {
+        const searchString = e.target.value;
+        setFilteredData(data.children.filter(
+          game => {
+            return (
+              game.children[0]
+              .value
+
+              .toLowerCase()
+              .includes(searchString.toLowerCase())
+            )
+          }
+        ))
+      };
+
 
     //   const getGameDetails = (gameid)=>{
 
@@ -65,7 +83,15 @@ function AllGames(){
         <>
             <h2 className='mochiy-pop-one-regular'>All Games</h2>
             <Link className='mochiy-pop-one-regular' to="/">Home</Link>
-            <div>{data.children.map(item => <Boardgame key={item.attributes.collid} {...{...item.children,...{"objectId":item.attributes.objectid}}}/>)}</div>
+            <div>
+            <input 
+              // className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+              type = "search" 
+              placeholder = "Search" 
+              onChange = {handleChange}
+            />
+            </div>
+            <div>{filteredData.map(item => <Boardgame key={item.attributes.collid} {...{...item.children,...{"objectId":item.attributes.objectid}}}/>)}</div>
             <div><Link className='mochiy-pop-one-regular' to="/">Home</Link></div>
         </>
         
